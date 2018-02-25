e91b2b287afd272b08b07746b4d66a3358c8e3d2
Simon Dolby <simon.dolby@yahoo.co.uk>
2018-02-25 18:34:58 +0000

    More Tests

---
+++ SCM Toolkit/.localhistory/SCM Toolkit Unit Tests/1519583692$PreferenceTests.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+Imports System.Windows.Forms
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class PreferenceTests
+
+    <TestMethod()>
+    Public Sub SaveSettings()
+
+        My.Settings.DatabaseLocationTest = "Hello this is literally a test"
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub LoadSettings()
+
+        MessageBox.Show(My.Settings.DatabaseLocationTest)
+
+    End Sub
+
+
+End Class
