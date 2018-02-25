c38b04491e871601a1d4531fde3a80bdb1ea9b31
Simon Dolby <simon.dolby@yahoo.co.uk>
2018-02-25 18:25:41 +0000

    Unit Tests

---
+++ SCM Toolkit/SCM Toolkit Unit Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SCM Toolkit Unit Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("SCM Toolkit Unit Tests")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c7a501bc-6ebd-4ee5-9bcc-2982c6942aac")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SCM Toolkit/SCM Toolkit Unit Tests/PreferenceTests.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class PreferenceTests
+
+    <TestMethod()>
+    Public Sub SaveSettings()
+
+        My.Settings.DatabaseLocationTest = "Hello this is literally a test"
+
+    End Sub
+
+
+End Class
